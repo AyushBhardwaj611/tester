d05c1912a390f546a5cde6b7eed94d62ff62e1a5
Ayush Bhardwaj <bhardwaj.ayush419@gmail.com>
2025-06-11 22:18:17 +0530

    first test

---
+++ src/main/java/org/example/controller/javaTestController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class javaTestController {
+    private int data;
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world1");
+    }
+
+}
